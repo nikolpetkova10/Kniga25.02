3a5de54a6ca76c7672b2b6b586e912df1cd56cf4
nikolpetkova10 <petkovaniki123@gmail.com>
2026-03-04 14:06:13 +0200

    Set minimum value for book price input

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if "books" not in st.session_state:
   st.header("Add a book")
   title = st.text_input("Title")
   author = st.text_input("Author")
-  price = st.number_input("Price")
+  price = st.number_input("Price", min_value=0.0)
 if st.button("Add the book"):
   book = {
     "Title": title,
