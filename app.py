7e80d3724d0e449bce582e9ef67996b10bfbe053
nikolpetkova10 <petkovaniki123@gmail.com>
2026-03-11 13:49:36 +0200

    Refactor book addition and display logic

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
 import streamlit as st
+
 st.title("Book")
+
 if "books" not in st.session_state:
-  st.session_state.books = []
-  st.header("Add a book")
-  title = st.text_input("Title")
-  author = st.text_input("Author")
-  price = st.number_input("Price", min_value=0.0)
+    st.session_state.books = []
+
+st.header("Add a book")
+title = st.text_input("Title")
+author = st.text_input("Author")
+price = st.number_input("Price", min_value=0.0)
+
 if st.button("Add the book"):
-  book = {
-    "Title": title,
-    "Author": author,
-    "Price": price
-  }
-  st.session_state.books.append(book)
-  st.success("The book is added")
+    book = {
+        "Title": title,
+        "Author": author,
+        "Price": price
+    }
+    st.session_state.books.append(book)
+    st.success("The book is added")
 if st.button("Show all books"):
-  if len(st.session_state.books) == 0:
-    st.write("There's no books added.")
-  else:
-    for book in st.session_state.books:
-      st.write("Title", book["Title"])
-      st.write("Author", book["Author"])
-      st.write("Price", book["Price"])
-      st.write("-----------------------")
+    if len(st.session_state.books) == 0:
+        st.write("There's no books added.")
+    else:
+        for book in st.session_state.books:
+            st.write("Title:", book["Title"])
+            st.write("Author:", book["Author"])
+            st.write("Price:", book["Price"])
+            st.write("-----------------------")
+
 st.header("Find by author")
 search_author = st.text_input("Enter the name of the author")
+
 if st.button("Find by author"):
-  found = False
-for books in st.session_state.books:
-  if book["author"] == search_author:
-    st.write(book)
-    found = True
-  if found == False:
-    st.write("There's no books by this author.")
-             
+    found = False
+    for book in st.session_state.books:
+        if book["Author"] == search_author:
+            st.write(book)
+            found = True
+
+    if not found:
+        st.write("There's no books by this author.")
       
       
 
